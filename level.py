f267f15d57edba6d9351bdce0b4a856136139f68
B33th0v3n960 <zhousun06@gmail.com>
2023-06-21 18:04:55 +0100

    Made the player movement and collision

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import pygame
 from tiles import Tile
-from settings import *
+from settings import tile_size, screen_width
 from player import Player
 
 
@@ -27,6 +27,46 @@ class Level:
                     player = Player((x, y))
                     self.player.add(player)
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width * 0.75 and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.rect.y += player.direction.y
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.direction.y = 0
+                elif player.direction.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.direction.y = 0
+
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+
     def run(self):
         # Draw tiles
         self.tiles.draw(self.display_surface)
@@ -35,3 +75,8 @@ class Level:
         # Draw Player
         self.player.draw(self.display_surface)
         self.player.update()
+
+        # Movement
+        self.horizontal_movement_collision()
+        self.vertical_movement_collision()
+        self.scroll_x()
