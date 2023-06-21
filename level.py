f65f6a31ea590f4732fdfff0bd24fb34b62cca04
B33th0v3n960 <zhousun06@gmail.com>
2023-06-21 12:36:39 +0100

    Created the player

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
 from settings import *
+from player import Player
 
 
 class Level:
@@ -12,16 +13,25 @@ class Level:
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
 
         # Iterate throught level_data and draw the map
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
                 if cell == "X":
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
+                elif cell == "P":
+                    player = Player((x, y))
+                    self.player.add(player)
 
     def run(self):
-        self.tiles.update(0)
+        # Draw tiles
         self.tiles.draw(self.display_surface)
+        self.tiles.update(self.world_shift)
+
+        # Draw Player
+        self.player.draw(self.display_surface)
+        self.player.update()
