4c756d1bb1de5a83e31668c92fb9b545f277fe19
B33th0v3n960 <zhousun06@gmail.com>
2023-06-23 15:30:18 +0100

    Created animation

--- level.py
+++ level.py
@@ -10,6 +10,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
         self.setup_level(level_data)
         self.world_shift = 0
+        self.current_x = 0
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -51,9 +52,15 @@ class Level:
                 if player.direction.y > 0:
                     player.rect.bottom = sprite.rect.top
                     player.direction.y = 0
+                    player.on_ground = True
                 elif player.direction.y < 0:
                     player.rect.top = sprite.rect.bottom
                     player.direction.y = 0
+                    player.on_ceiling = True
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1:
+            player.on_ground = False
+        if player.on_ceiling and player.direction.y > 1:
+            player.on_ground = False
 
     def horizontal_movement_collision(self):
         player = self.player.sprite
@@ -64,8 +71,21 @@ class Level:
             if sprite.rect.colliderect(player.rect):
                 if player.direction.x < 0:
                     player.rect.left = sprite.rect.right
+                    player.on_left = True
+                    self.current_x = player.rect.left
                 elif player.direction.x > 0:
                     player.rect.right = sprite.rect.left
+                    player.on_right = True
+                    self.current_x = player.rect.right
+
+        if player.on_left and (
+            player.rect.left < self.current_x or player.direction.x >= 0
+        ):
+            player.on_left = False
+        if player.on_right and (
+            player.rect.left > self.current_x or player.direction.x <= 0
+        ):
+            player.on_right = False
 
     def run(self):
         # Draw tiles
